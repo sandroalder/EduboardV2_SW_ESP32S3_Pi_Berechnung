a558379486662060351102160564498522e177b8
Sandro Alder, T-CH <sandro.alder@tecan.com>
2025-11-12 16:00:14 +0100

    Pi algorythmus eingefügt

--- src/main.c
+++ src/main.c
@@ -1,38 +1,109 @@
 /********************************************************************************************* */
-//    Eduboard2 ESP32-S3 Template with BSP
-//    Author: Martin Burger
+//    Pi calculation
+//    Author: Sandro Alder
 //    Juventus Technikerschule
 //    Version: 1.0.0
 //    
-//    This is the ideal starting point for a new Project. BSP for most of the Eduboard2
-//    Hardware is included under components/eduboard2.
-//    Hardware support can be activated/deactivated in components/eduboard2/eduboard2_config.h
 /********************************************************************************************* */
+
+
 #include "eduboard2.h"
 #include "memon.h"
-
 #include "math.h"
 
-#define TAG "TEMPLATE"
 
-  // double hat auf ESP32 64-Bit Präzision
-static double Leibniz_sum = 0.0;
+#include <stdlib.h>          
+#include "esp_system.h"      
 
+
+/*********************************************************************************************
+ *   Leibnitz Reihe
+ *********************************************************************************************/
+
+static double Leibniz_Pi = 0.0;
 
 void LeibnizTask(void *pvParameters) {
   (void) pvParameters;
     
-  uint64_t k = 0;
+    static bool add = (true);
+    static double Leibniz_sum = 0.0;
+    uint64_t k = 1;
+
+    while (1) {
+
+    vTaskDelay(pdMS_TO_TICKS(1));
+        
+        if (add)
+        {
+            add = false;
+            Leibniz_sum += 1/(double)k;
+        }
+        else
+        {
+            add = true;
+            Leibniz_sum -= 1/(double)k;
+        }
+        k++;
+        k++;
+
+        Leibniz_Pi = Leibniz_sum*4;
+    }
+}
+
+/*********************************************************************************************
+ *   Wallissches Produkt
+ *********************************************************************************************/
+
+static double Wallis_Pi = 0.0;
+
+void WallisTask(void *pvParameters) {
+  (void) pvParameters;
+    
+    static double Wallis_prod = 1.0;
+    uint64_t k = 2;
 
     while (1) {
         
-        // Berechne Term und addiere
-        // term = (-1)^k / (2k+1)
-        double denom = 2.0 * (double)k + 1.0;
-        double term = ((k & 1ULL) == 0ULL) ? (1.0 / denom) : (-1.0 / denom);
-        Leibniz_sum += term;
+        vTaskDelay(pdMS_TO_TICKS(1));
 
-        taskYIELD();
+        Wallis_prod *= ((double)k/ ((double)k-1)) * ((double)k/ ((double)k+1));
+        k++;
+        k++;
+        
+        Wallis_Pi = Wallis_prod*2;
+
+
+    }
+}
+
+
+
+/*********************************************************************************************
+ *   LCD update
+ *********************************************************************************************/
+
+void LCD_update(void* param) {
+    
+    
+    while(1) {
+    
+        vTaskDelay(pdMS_TO_TICKS(500));
+        lcdFillScreen(BLACK);
+
+        lcdDrawString(fx32M, 10, 30, "Pi calculation", WHITE);
+        
+        char Pi_string[32];
+        //Leibnitz
+        sprintf(Pi_string, "%.10f", Leibniz_Pi);
+        lcdDrawString(fx24M, 10, 100, "Leibnitz", WHITE);
+        lcdDrawString(fx24M, 200, 100, Pi_string, WHITE);
+        //Wallis
+        sprintf(Pi_string, "%.10f", Wallis_Pi);
+        lcdDrawString(fx24M, 10, 200, "Wallis", WHITE);
+        lcdDrawString(fx24M, 200, 200, Pi_string, WHITE);
+
+        lcdUpdateVScreen();
+
     }
 }
 
@@ -48,11 +119,28 @@ void app_main()
                 "LeibnizTask",     //Name
                 2*2048,         //Stacksize
                 NULL,           //Parameters
+                5,             //Priority
+                NULL);          //Taskhandle
+
+    //Create WallisTask
+    xTaskCreate(WallisTask,   //Subroutine
+                "WallisTask",     //Name
+                2*2048,         //Stacksize
+                NULL,           //Parameters
+                5,             //Priority
+                NULL);          //Taskhandle
+
+    //Create LCD Update
+    xTaskCreate(LCD_update,   //Subroutine
+                "LCD_update",     //Name
+                2*2048,         //Stacksize
+                NULL,           //Parameters
                 10,             //Priority
                 NULL);          //Taskhandle
+
+
     for(;;) {
 
         vTaskDelay(2000/portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "Pi_calculation");
     }
 }
