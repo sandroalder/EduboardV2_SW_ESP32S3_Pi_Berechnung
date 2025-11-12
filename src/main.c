da622b84bccc9c84ce736a6563a1833da91702b7
Sandro Alder, T-CH <sandro.alder@tecan.com>
2025-11-12 11:06:39 +0100

    Leibniz Task added

--- src/main.c
+++ src/main.c
@@ -15,36 +15,44 @@
 
 #define TAG "TEMPLATE"
 
-#define UPDATETIME_MS 100
+  // double hat auf ESP32 64-Bit Präzision
+static double Leibniz_sum = 0.0;
 
-void templateTask(void* param) {
-    //Init stuff here
-    vTaskDelay(100);
-    for(;;) {
-        // task main loop
-        if(button_get_state(SW0, true) == SHORT_PRESSED) {
-            led_toggle(LED0);
-        }
-        led_toggle(LED7);
-        // delay
-        vTaskDelay(UPDATETIME_MS/portTICK_PERIOD_MS);
+
+void LeibnizTask(void *pvParameters) {
+  (void) pvParameters;
+    
+  uint64_t k = 0;
+
+    while (1) {
+        
+        // Berechne Term und addiere
+        // term = (-1)^k / (2k+1)
+        double denom = 2.0 * (double)k + 1.0;
+        double term = ((k & 1ULL) == 0ULL) ? (1.0 / denom) : (-1.0 / denom);
+        Leibniz_sum += term;
+
+        taskYIELD();
     }
 }
+
+
 
 void app_main()
 {
     //Initialize Eduboard2 BSP
     eduboard2_init();
     
-    //Create templateTask
-    xTaskCreate(templateTask,   //Subroutine
-                "testTask",     //Name
+    //Create LeibnizTask
+    xTaskCreate(LeibnizTask,   //Subroutine
+                "LeibnizTask",     //Name
                 2*2048,         //Stacksize
                 NULL,           //Parameters
                 10,             //Priority
                 NULL);          //Taskhandle
     for(;;) {
+
         vTaskDelay(2000/portTICK_PERIOD_MS);
-        ESP_LOGI(TAG, "Hello Eduboard");
+        ESP_LOGI(TAG, "Pi_calculation");
     }
 }
