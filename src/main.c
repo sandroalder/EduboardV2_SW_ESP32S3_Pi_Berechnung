9dc99d6fa0fe74898dfc4cb4307be93e57449143
Sandro Alder, T-CH <sandro.alder@tecan.com>
2025-11-12 18:28:00 +0100

    Steuertask eingefügt

--- src/main.c
+++ src/main.c
@@ -16,6 +16,50 @@
 #include "esp_system.h"      
 
 
+// Zustände der State Machine
+typedef enum {
+    Idle,
+    Run_Leibniz,
+    Run_Wallis,
+    Reset
+} State_t;
+
+static volatile State_t state = Idle;
+static volatile bool Leibniz_Reset = false;
+static volatile bool Wallis_Reset = false;
+
+/*********************************************************************************************
+ *   SteuerTask
+ *********************************************************************************************/
+
+void Steuertask(void* param) {
+    
+    while(1) {
+    
+        vTaskDelay(pdMS_TO_TICKS(100));
+        
+        if(button_get_state(SW0, true) == SHORT_PRESSED) {
+            state = Idle;
+        }
+        if(button_get_state(SW1, true) == SHORT_PRESSED) {
+             state = Run_Leibniz;
+        }
+        if(button_get_state(SW2, true) == SHORT_PRESSED) {
+            state = Run_Wallis;
+        }
+        if(button_get_state(SW3, true) == SHORT_PRESSED) {
+            state = Reset;
+            while (!(Leibniz_Reset && Wallis_Reset)){
+                vTaskDelay(pdMS_TO_TICKS(10));      // Warte bis Reset abgeschlossen
+            }  
+            Leibniz_Reset = false;
+            Wallis_Reset = false;
+            state = Idle;
+        }
+
+    }
+}
+
 /*********************************************************************************************
  *   Leibnitz Reihe
  *********************************************************************************************/
@@ -29,24 +73,39 @@ void LeibnizTask(void *pvParameters) {
     static double Leibniz_sum = 0.0;
     uint64_t k = 1;
 
-    while (1) {
+    while(1)
+    {
+        if (state == Idle)
+            vTaskDelay(pdMS_TO_TICKS(100));
 
-    vTaskDelay(pdMS_TO_TICKS(1));
-        
-        if (add)
-        {
-            add = false;
-            Leibniz_sum += 1/(double)k;
-        }
-        else
-        {
-            add = true;
-            Leibniz_sum -= 1/(double)k;
-        }
-        k++;
-        k++;
+        while (state == Run_Leibniz) {
 
-        Leibniz_Pi = Leibniz_sum*4;
+        vTaskDelay(pdMS_TO_TICKS(1));
+            
+            if (add)
+            {
+                add = false;
+                Leibniz_sum += 1/(double)k;
+            }
+            else
+            {
+                add = true;
+                Leibniz_sum -= 1/(double)k;
+            }
+            k++;
+            k++;
+
+            Leibniz_Pi = Leibniz_sum*4;
+        }
+
+        if (state == Reset)
+        {
+            Leibniz_Pi = 0.0;
+            Leibniz_sum = 0.0;
+            add = (true);
+            k = 1;
+            Leibniz_Reset = true;
+        }
     }
 }
 
@@ -62,21 +121,34 @@ void WallisTask(void *pvParameters) {
     static double Wallis_prod = 1.0;
     uint64_t k = 2;
 
-    while (1) {
-        
-        vTaskDelay(pdMS_TO_TICKS(1));
+    while(1)
+    {
+        if (state == Idle)
+            vTaskDelay(pdMS_TO_TICKS(100));
+       
+        while (state == Run_Wallis) {
 
-        Wallis_prod *= ((double)k/ ((double)k-1)) * ((double)k/ ((double)k+1));
-        k++;
-        k++;
-        
-        Wallis_Pi = Wallis_prod*2;
+            vTaskDelay(pdMS_TO_TICKS(1));
 
+            Wallis_prod *= ((double)k/ ((double)k-1)) * ((double)k/ ((double)k+1));
+            k++;
+            k++;
+            
+            Wallis_Pi = Wallis_prod*2;
+        }
 
+        if (state == Reset)
+        {
+            Wallis_Pi = 0.0;
+            Wallis_prod = 1.0;
+            k = 2;
+            Wallis_Reset = true;
+        }
     }
 }
 
 
+ 
 
 /*********************************************************************************************
  *   LCD update
@@ -114,25 +186,34 @@ void app_main()
     //Initialize Eduboard2 BSP
     eduboard2_init();
     
-    //Create LeibnizTask
-    xTaskCreate(LeibnizTask,   //Subroutine
-                "LeibnizTask",     //Name
+    //Create SteuertTask
+    xTaskCreate(Steuertask,     //Subroutine
+                "Steuertask",   //Name
                 2*2048,         //Stacksize
                 NULL,           //Parameters
-                5,             //Priority
+                15,             //Priority
+                NULL);          //Taskhandle
+
+
+    //Create LeibnizTask
+    xTaskCreate(LeibnizTask,    //Subroutine
+                "LeibnizTask",  //Name
+                2*2048,         //Stacksize
+                NULL,           //Parameters
+                5,              //Priority
                 NULL);          //Taskhandle
 
     //Create WallisTask
-    xTaskCreate(WallisTask,   //Subroutine
-                "WallisTask",     //Name
-                2*2048,         //Stacksize
-                NULL,           //Parameters
-                5,             //Priority
-                NULL);          //Taskhandle
+    xTaskCreate(WallisTask,      //Subroutine
+                "WallisTask",    //Name
+                2*2048,          //Stacksize
+                NULL,            //Parameters
+                5,               //Priority
+                NULL);           //Taskhandle
 
     //Create LCD Update
-    xTaskCreate(LCD_update,   //Subroutine
-                "LCD_update",     //Name
+    xTaskCreate(LCD_update,     //Subroutine
+                "LCD_update",   //Name
                 2*2048,         //Stacksize
                 NULL,           //Parameters
                 10,             //Priority
